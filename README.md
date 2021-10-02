53ecf5b3b13af13ed38a6916ad8459d73fabde75
Shou <shou@disroot.org>
2021-10-02 21:54:08 +0530

    Add switch-a and switch-b alias for bootctl

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Drodian Tweaks
+# Droidian Tweaks
 
-These tweaks are specifically for jasmine/wayne. At some point these fixes should be turned into device adaptation. Running `setup.sh` file fixes the following bugs:
+These tweaks are specifically for jasmine/wayne. At some point these fixes should be turned into device adaptation package. Running `setup.sh` file fixes the following bugs:
 
 1. Brightness (You should disable automatic brightness from settings>power in gnome settings)
 2. Bluetooth
 3. Scaling
 
-The `setup.sh` script additionally installs and enables flathub and kdeapps flathub repos as well.
+The `setup.sh` script additionally installs and enables flathub and kdeapps flathub repos and sets `switch-a` and `switch-b` bootctl alias too.
 
-`wget https://github.com/Droidian-Mi-A2-6X/jasmine-tweaks/blob/master/setup.sh && chmod +x setup.sh && ./setup.sh`
+`wget https://github.com/Droidian-Mi-A2-6X/droidian-tweaks/raw/master/setup.sh && chmod +x setup.sh && ./setup.sh`
 
-For waydroid run `wget https://github.com/Droidian-Mi-A2-6X/jasmine-tweaks/blob/master/waydroid-setup.sh && chmod +x waydroid-setup.sh && ./waydroid-setup.sh`
+For waydroid (work in progress!) run `wget https://github.com/Droidian-Mi-A2-6X/jasmine-tweaks/blob/master/waydroid-setup.sh && chmod +x waydroid-setup.sh && ./waydroid-setup.sh`
--- setup.sh
+++ setup.sh
@@ -8,17 +8,22 @@ echo "Downloading fixes..."
 
 sudo mkdir /lib/systemd/system/bluebinder.service.d/
 
-sudo wget https://raw.githubusercontent.com/Droidian-Mi-A2-6X/jasmine-tweaks/master/lib/systemd/system/bluebinder.service.d/90-jasmine.conf -O /lib/systemd/system/bluebinder.service.d/90-jasmine.conf && sudo chmod 0644 /lib/systemd/bluebinder.service.d/90-jasmine.conf
+sudo wget https://raw.githubusercontent.com/Droidian-Mi-A2-6X/droidian-tweaks/master/lib/systemd/system/bluebinder.service.d/90-jasmine.conf -O /lib/systemd/system/bluebinder.service.d/90-jasmine.conf && sudo chmod 0644 /lib/systemd/bluebinder.service.d/90-jasmine.conf
 
-sudo wget https://raw.githubusercontent.com/Droidian-Mi-A2-6X/jasmine-tweaks/master/lib/systemd/system/brightness-fix.service -O /lib/systemd/system/brightness-fix.service && sudo chmod 0644 /lib/systemd/system/brightness-fix.service
+sudo wget https://raw.githubusercontent.com/Droidian-Mi-A2-6X/droidian-tweaks/master/lib/systemd/system/brightness-fix.service -O /lib/systemd/system/brightness-fix.service && sudo chmod 0644 /lib/systemd/system/brightness-fix.service
 
 sudo systemctl daemon-reload && sudo systemctl enable bluebinder.service && sudo systemctl enable brightness-fix.service && sudo systemctl start brightness-fix.service
 
-sudo rm /usr/share/phosh/phoc.ini && sudo wget https://github.com/Droidian-Mi-A2-6X/jasmine-tweaks/raw/master/usr/share/phosh/phoc.ini -O /usr/share/phosh/phoc.ini && sudo chmod 0644 /usr/share/phosh/phoc.ini
+sudo rm /usr/share/phosh/phoc.ini && sudo wget https://github.com/Droidian-Mi-A2-6X/droidian-tweaks/raw/master/usr/share/phosh/phoc.ini -O /usr/share/phosh/phoc.ini && sudo chmod 0644 /usr/share/phosh/phoc.ini
 
 echo "Setting up flatpak repo(s) for convenience..."
 
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak remote-add --user --if-not-exists kdeapps http://distribute.kde.org/kdeapps.flatpakrepo
+
+echo "Setting up alias for bootctl..."
+
+echo 'alias switch-a="unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl set-active-boot-slot 0"' >> ~/.bashrc
+echo 'alias switch-b="unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl set-active-boot-slot 1"' >> ~/.bashrc
 
 echo "Rebooting now."
 
